c8a518e41c12cd84fcf24fd59b7194fe7489ca0c
Maheshperuru <perurumahesh4@gmail.com>
2026-03-17 14:06:03 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,27 @@
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+        int number;
+        double price;
+        char grade;
+        String name;
+number = 10;
+ price = 50.5;
+ grade = 'B';
+  name = "Mahesh";
+  System.out.println("Initial Values:");
+  System.out.println("Number: " + number);
+  System.out.println("Price: " + price);
+  System.out.println("Grade: " + grade);
+  System.out.println("Name: " + name);
+ number = 20;
+    price = 75.9;
+    grade = 'A';
+    name = "Java";
+      System.out.println("\nUpdated Values:");
+        System.out.println("Number: " + number);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Name: " + name);
+    }
+}

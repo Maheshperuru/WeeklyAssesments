450b006f47e898c1edad687c9f4b958fe000af5a
Maheshperuru <perurumahesh4@gmail.com>
2026-03-17 14:02:31 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,13 @@
+public class Datatypes {
+
+    public static void main(String[] args) {
+ int number = 25;
+ double price = 99.99;
+char grade = 'A';
+ String name = "Mahesh";
+   System.out.println("Integer value: " + number);
+        System.out.println("Double value: " + price);
+        System.out.println("Character value: " + grade);
+        System.out.println("String value: " + name);
+    }
+}

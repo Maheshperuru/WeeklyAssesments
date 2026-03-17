0cb7fcaf31a6adac5b90f3da06df97ee98e2d7d6
Maheshperuru <perurumahesh4@gmail.com>
2026-03-17 13:59:42 +0530

    Create Comments.java

---
+++ Comments.java
@@ -0,0 +1,16 @@
+public class Comments {
+
+    public static void main(String[] args) {
+
+        // single line comment
+        int a = 10; // variable a
+
+        int b = 20; // variable b
+
+        /* Multi-line comment
+           Adding two numbers */
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
